87e9e05f43b4c734a19b84b98ef37ba51804e3ca
AdamJaidiEhb <adam.jaidi@student.ehb.be>
2024-12-11 12:01:13 +0100

    Update WorkerAccountDetails.js

---
+++ screens/WorkerAccountDetails.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+
+export default function AccountDetails() 
+{
+    
+}
